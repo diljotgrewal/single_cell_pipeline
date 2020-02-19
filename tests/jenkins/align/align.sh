702fd75d93101486a7b57549923179f1a893ba77
Diljot Grewal <diljot.grewal@gmail.com>
2020-02-19 11:24:48 -0500

    minor

--- tests/jenkins/align/align.sh
+++ tests/jenkins/align/align.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-ORG=$1
-
 TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
 
 
 mkdir -p ALIGN/tumourfastq
 
-docker run -v $PWD:$PWD -w $PWD $ORG/azurecli:v0.0.1 \
+docker run -v $PWD:$PWD -w $PWD $3/azurecli:v0.0.1 \
   az storage blob download-batch -s tumourfastq -d ALIGN/tumourfastq --account-name $1 --account-key $2
 
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v /usr/bin/docker:/usr/bin/docker --rm \
-  $ORG/single_cell_pipeline:$TAG \
+  $3/single_cell_pipeline:$TAG \
   single_cell alignment --input_yaml tests/jenkins/align/inputs.yaml \
   --library_id A97318A --maxjobs 4 --nocleanup --sentinel_only  \
   --context_config tests/jenkins/align/context_config.yaml \
@@ -24,8 +22,8 @@ docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/va
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v /usr/bin/docker:/usr/bin/docker --rm \
-  $ORG/single_cell_pipeline:$TAG \
+  $3/single_cell_pipeline:$TAG \
   python tests/jenkins/align/test_alignment.py ALIGN/output A97318A  ALIGN/ref_test_data/refdata
 
 
-docker run -w $PWD -v $PWD:$PWD --rm $ORG/single_cell_pipeline:$TAG rm -rf ALIGN
+docker run -w $PWD -v $PWD:$PWD --rm $3/single_cell_pipeline:$TAG rm -rf ALIGN
